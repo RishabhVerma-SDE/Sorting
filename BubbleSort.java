67962134180aa53c0e1771a42c187efb491cb0e4
Saloni Verma <saloni@Salonis-MacBook-Air.local>
2024-02-12 18:36:51 +0530

    bubble sort

---
+++ BubbleSort.java
@@ -0,0 +1,34 @@
+public class BubbleSort {
+
+
+
+    public static void printArray(int[] arr){
+        int n = arr.length;
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static int[] sortArray(int[] arr){
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++){
+            for (int j = 0; j < n-i-1; j++){
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+
+    public static void main(String[] args){
+
+        int[] arr = {2,5,3,7,5,9,10};
+
+        printArray(sortArray(arr));
+   
+    }
+}
